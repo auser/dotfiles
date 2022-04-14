9cff9aa8440a973d7ce38b32e8617906bc5a6c7c
auser <me@ari.io>
2022-04-14 17:34:11 -0400

    Removed nix

--- home/private_dot_config/nixpkgs/modules/linux.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    gcc # neovim - building nvim-treesitter parsers
-    gnumake # neovim treesitter
-    nq # linux queue utility
-    unzip # wsl doesn't include unzip
-  ];
-}
