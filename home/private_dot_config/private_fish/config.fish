55cd824353d97a3c9f7aa1b4b3080d3b548293d8
Ari <me@ari.io>
2022-08-03 09:33:08 -0400

    Added starship

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -4,3 +4,5 @@ end
 # https://gist.github.com/gagarine/cf3f65f9be6aa0e105b184376f765262
 set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
 set -g fish_complete_path $fish_complete_path /opt/homebrew/share/fish/vendor_completions.d
+starship init fish | source
+
