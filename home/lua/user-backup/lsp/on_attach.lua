a8ba61a60c5abf95d99c636647a0ead2cdccacd0
Ari <me@ari.io>
2022-11-17 11:22:55 -0500

    WIP

--- home/lua/user-backup/lsp/on_attach.lua
+++
@@ -1,9 +0,0 @@
-return function(client, bufnr)
-  if client.name == "sqls" then
-    require("sqls").on_attach(client, bufnr)
-  end
-
-  if client.name == "solargraph" then
-    client.server_capabilities.document_formatting = false
-  end
-end
