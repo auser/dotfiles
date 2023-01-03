c6651491c9054a42a39f22e6e882af8c100a6e6b
Ari <me@ari.io>
2023-01-03 03:52:52 -0500

    Updates for nodenv

--- home/private_dot_config/zsh/env.zsh
+++ home/private_dot_config/zsh/env.zsh
@@ -21,6 +21,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
+# place this after nvm initialization!
+eval "$(nodenv init -)"
+
 export PNPM_HOME="/Users/auser/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 
