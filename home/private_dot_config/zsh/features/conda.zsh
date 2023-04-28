21d424468895166027d82db41ef5df176487a1a8
Ari <me@ari.io>
2023-04-27 17:21:54 -0700

    Updated chezmoi

--- home/private_dot_config/zsh/features/conda.zsh
+++ home/private_dot_config/zsh/features/conda.zsh
@@ -15,15 +15,16 @@
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/Users/auser/miniforge3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh" ]; then
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/profile.d/conda.sh"
+    if [ -f "/Users/auser/miniforge3/etc/profile.d/conda.sh" ]; then
+        . "/Users/auser/miniforge3/etc/profile.d/conda.sh"
     else
-        export PATH="/opt/homebrew/Caskroom/miniconda/base/bin:$PATH"
+        export PATH="/Users/auser/miniforge3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
+
