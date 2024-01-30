0d88a8df6d452c4f15b61fef6351c14f3c7de54b
Ari <me@ari.io>
2024-01-30 07:52:04 -0800

    Slimmed

--- home/private_dot_config/zsh/env.zsh
+++ home/private_dot_config/zsh/env.zsh
@@ -4,36 +4,12 @@
 export EDITOR='nvim'
 export SUDO_EDITOR='nvim'
 
-# PATH
-# if command -v pyenv 1>/dev/null 2>&1; then
-#     export PATH="~/.pyenv/bin:$PATH"
-#     export PYENV_ROOT="$HOME/.pyenv"
-#     export PATH="$PYENV_ROOT/bin:$PATH"
-#     eval "$(pyenv init --path)"
-# fi
-
-# if command -v pyenv-virtualenv &>/dev/null; then
-#     eval "$(pyenv virtualenv-init -)"
-# fi
-
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
-# place this after nvm initialization!
-#eval "$(nodenv init -)"
 
 export PNPM_HOME="/Users/auser/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 export PATH=~/.mix/escripts:$PATH
-
-# ASDF
-# . $(brew --prefix asdf)/libexec/asdf.sh
-
-# RTX
-# eval "$(brew --prefix rtx) activate zsh"
-# eval "$(rtx activate zsh)"
-
-# mise
-eval "$(~/.local/bin/mise activate zsh)"
--- home/private_dot_config/zsh/features/02_asdf.zsh
+++ home/private_dot_config/zsh/features/02_asdf.zsh
@@ -1,15 +1,8 @@
-if [ -d $HOME/.asdk  ]; then
-#export PATH=~/.asdf/bin:$PATH
-#. $HOME/.asdf/asdf.sh
-# . $(brew --prefix asdf)/asdf.sh
-fi
-
-# curl https://rtx.pub/rtx-latest-macos-arm64 > ~/.bin/rtx
-export PATH="$HOME/.local/share/rtx/shims:$PATH"
-# eval "$($HOME/.bin/rtx activate zsh)"
-
-eval "$(brew --prefix rtx) activate zsh)"
-# chmod +x ~/.bin/rtx
-
 export PATH="$HOME/.bin:$PATH"
 
+# mise
+if [[ -o interactive ]]; then
+  eval "$(mise activate zsh)"
+else
+  eval "$(mise activate zsh --shims)"
+fi
