90d64dd331ec7ba08980b985fc168802885e568c
Ari <me@ari.io>
2024-02-29 10:19:17 -0800

    WIP

--- home/private_dot_config/zsh/env.zsh
+++ home/private_dot_config/zsh/env.zsh
@@ -5,9 +5,9 @@ export EDITOR='nvim'
 export SUDO_EDITOR='nvim'
 
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+# [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 
 export PNPM_HOME="/Users/auser/Library/pnpm"
