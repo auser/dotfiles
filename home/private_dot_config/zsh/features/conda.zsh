aab0a6810a23e8d43cbcfe2eb1241bc1151179ae
Ari <me@ari.io>
2026-08-03 16:10:04 -0700

    Make conda setup optional

--- home/private_dot_config/zsh/features/conda.zsh
+++ home/private_dot_config/zsh/features/conda.zsh
@@ -1,14 +1,22 @@
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/homebrew/Caskroom/miniforge/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
-        . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
+# Conda is optional. Prefer an existing PATH entry, then check common installs.
+_conda_exe="$(command -v conda 2>/dev/null)" || _conda_exe=""
+if [[ -z "$_conda_exe" ]]; then
+  for _candidate in \
+    /opt/homebrew/Caskroom/miniforge/base/bin/conda \
+    "$HOME/miniforge3/bin/conda" \
+    "$HOME/mambaforge/bin/conda" \
+    "$HOME/anaconda3/bin/conda"
+  do
+    if [[ -x "$_candidate" ]]; then
+      _conda_exe="$_candidate"
+      break
     fi
+  done
 fi
-unset __conda_setup
-# <<< conda initialize <<<
+
+if [[ -x "$_conda_exe" ]]; then
+  __conda_setup="$("$_conda_exe" shell.zsh hook 2>/dev/null)" || __conda_setup=""
+  [[ -n "$__conda_setup" ]] && eval "$__conda_setup"
+  unset __conda_setup
+fi
+unset _candidate _conda_exe
